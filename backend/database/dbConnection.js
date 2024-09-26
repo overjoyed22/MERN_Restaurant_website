be81c43422faca2b0bda94d497ce1f3c02a92d24
overjoyed22 <pulkitnits2022@gmail.com>
2024-09-26 06:09:04 +0530

    Update dbConnection.js

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -5,6 +5,7 @@ export const dbConnection = ()=>{
         dbName: "RESTAURANT"
     })
     .then(()=>{
+        // a change 
         console.log("Connected to database successfully!");
     })
     .catch((err)=>{
